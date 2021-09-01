bc1a4f2a7cb4313c7c397ff6feeee10fcbaea951
saurav <sauravsinghrauthan@gmail.com>
2021-09-01 21:05:36 +0530

    added svg responsiveness

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,17 +1,31 @@
 let controller = new ScrollMagic.Controller();
 
+var bar = new ProgressBar.Path('#Opaque_Ring', {
+    easing: 'easeInOut',
+    duration: 800
+});
+  
+
 // page2
 new ScrollMagic.Scene({
   triggerElement: ".trig1",
 })
   .setTween(".mainText", 1, { backgroundColor: "#213046" })
-  .addTo(controller);
+  .addTo(controller)
+  .on("enter", () => {
+    bar.set(0);
+    bar.animate(.16);
+  });
 
 new ScrollMagic.Scene({
   triggerElement: ".trig1",
 })
   .setTween(".svgCont", 1, { backgroundColor: "#213046" })
-  .addTo(controller);
+  .addTo(controller)
+  .on('leave',() => {
+      bar.set(0.16);
+      bar.animate(0.0);
+  });
 
 new ScrollMagic.Scene({
   triggerElement: ".trig1",
@@ -24,13 +38,21 @@ new ScrollMagic.Scene({
   triggerElement: ".trig2",
 })
   .setTween(".mainText", 1, { backgroundColor: "rgb(61, 6, 68)" })
-  .addTo(controller);
+  .addTo(controller)
+  .on("enter", () => {
+    bar.set(0.16);
+    bar.animate(.35);
+  });
 
 new ScrollMagic.Scene({
   triggerElement: ".trig2",
 })
   .setTween(".svgCont", 1, { backgroundColor: "rgb(61, 6, 68)" })
-  .addTo(controller);
+  .addTo(controller)
+  .on('leave',() => {
+    bar.set(0.35);
+    bar.animate(0.16);
+});
 
 new ScrollMagic.Scene({
   triggerElement: ".trig2",
@@ -43,13 +65,21 @@ new ScrollMagic.Scene({
   triggerElement: ".trig3",
 })
   .setTween(".mainText", 1, { backgroundColor: "rgba(204, 158, 9)" })
-  .addTo(controller);
+  .addTo(controller)
+  .on("enter", () => {
+    bar.set(0.35);
+    bar.animate(.5);
+  });
 
 new ScrollMagic.Scene({
   triggerElement: ".trig3",
 })
   .setTween(".svgCont", 1, { backgroundColor: "rgba(204, 158, 9)" })
-  .addTo(controller);
+  .addTo(controller)
+  .on("leave", () => {
+    bar.set(0.5);
+    bar.animate(.35);
+  });
 
 new ScrollMagic.Scene({
   triggerElement: ".trig3",
@@ -62,13 +92,21 @@ new ScrollMagic.Scene({
   triggerElement: ".trig4",
 })
   .setTween(".mainText", 1, { backgroundColor: "rgb(45, 17, 63)" })
-  .addTo(controller);
+  .addTo(controller)
+  .on("enter", () => {
+    bar.set(0.5);
+    bar.animate(.65);
+  });
 
 new ScrollMagic.Scene({
   triggerElement: ".trig4",
 })
   .setTween(".svgCont", 1, { backgroundColor: "rgb(45, 17, 63)" })
-  .addTo(controller);
+  .addTo(controller)
+  .on("leave", () => {
+    bar.set(0.65);
+    bar.animate(.5);
+  });
 
 new ScrollMagic.Scene({
   triggerElement: ".trig4",
@@ -81,13 +119,21 @@ new ScrollMagic.Scene({
   triggerElement: ".trig5",
 })
   .setTween(".mainText", 1, { backgroundColor: "rgb(124, 39, 79)" })
-  .addTo(controller);
+  .addTo(controller)
+  .on("enter", () => {
+    bar.set(0.65);
+    bar.animate(.83);
+  });
 
 new ScrollMagic.Scene({
   triggerElement: ".trig5",
 })
   .setTween(".svgCont", 1, { backgroundColor: "rgb(124, 39, 79)" })
-  .addTo(controller);
+  .addTo(controller)
+  .on("leave", () => {
+    bar.set(0.83);
+    bar.animate(.65);
+  });
 
 new ScrollMagic.Scene({
   triggerElement: ".trig5",
@@ -100,13 +146,21 @@ new ScrollMagic.Scene({
   triggerElement: ".trig6",
 })
   .setTween(".mainText", 1, { backgroundColor: "rgb(33, 85, 56)" })
-  .addTo(controller);
+  .addTo(controller)
+  .on("enter", () => {
+    bar.set(0.83);
+    bar.animate(1);
+  });
 
 new ScrollMagic.Scene({
   triggerElement: ".trig6",
 })
   .setTween(".svgCont", 1, { backgroundColor: "rgb(33, 85, 56)" })
-  .addTo(controller);
+  .addTo(controller)
+  .on("leave", () => {
+    bar.set(1);
+    bar.animate(.83);
+  });
 
 new ScrollMagic.Scene({
   triggerElement: ".trig6",
