7c477ee55c2215f5db55ed33d2c4dad483e9ac87
saurav <sauravsinghrauthan@gmail.com>
2021-09-01 20:44:04 +0530

    desktop view adde w color change

---
+++ scripts/animation.js
@@ -0,0 +1,115 @@
+let controller = new ScrollMagic.Controller();
+
+// page2
+new ScrollMagic.Scene({
+  triggerElement: ".trig1",
+})
+  .setTween(".mainText", 1, { backgroundColor: "#213046" })
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: ".trig1",
+})
+  .setTween(".svgCont", 1, { backgroundColor: "#213046" })
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: ".trig1",
+})
+  .setTween(".mainImg", 1, { backgroundColor: "#121a26" })
+  .addTo(controller);
+
+// page3
+new ScrollMagic.Scene({
+  triggerElement: ".trig2",
+})
+  .setTween(".mainText", 1, { backgroundColor: "rgb(61, 6, 68)" })
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: ".trig2",
+})
+  .setTween(".svgCont", 1, { backgroundColor: "rgb(61, 6, 68)" })
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: ".trig2",
+})
+  .setTween(".mainImg", 1, { backgroundColor: "rgb(121, 3, 121)" })
+  .addTo(controller);
+
+// page4
+new ScrollMagic.Scene({
+  triggerElement: ".trig3",
+})
+  .setTween(".mainText", 1, { backgroundColor: "rgba(204, 158, 9)" })
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: ".trig3",
+})
+  .setTween(".svgCont", 1, { backgroundColor: "rgba(204, 158, 9)" })
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: ".trig3",
+})
+  .setTween(".mainImg", 1, { backgroundColor: "rgba(170, 132, 6)" })
+  .addTo(controller);
+
+// page5
+new ScrollMagic.Scene({
+  triggerElement: ".trig4",
+})
+  .setTween(".mainText", 1, { backgroundColor: "rgb(45, 17, 63)" })
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: ".trig4",
+})
+  .setTween(".svgCont", 1, { backgroundColor: "rgb(45, 17, 63)" })
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: ".trig4",
+})
+  .setTween(".mainImg", 1, { backgroundColor: "rgb(98, 4, 160)" })
+  .addTo(controller);
+
+// page6
+new ScrollMagic.Scene({
+  triggerElement: ".trig5",
+})
+  .setTween(".mainText", 1, { backgroundColor: "rgb(124, 39, 79)" })
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: ".trig5",
+})
+  .setTween(".svgCont", 1, { backgroundColor: "rgb(124, 39, 79)" })
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: ".trig5",
+})
+  .setTween(".mainImg", 1, { backgroundColor: "rgb(223, 67, 140)" })
+  .addTo(controller);
+
+// page7
+new ScrollMagic.Scene({
+  triggerElement: ".trig6",
+})
+  .setTween(".mainText", 1, { backgroundColor: "rgb(33, 85, 56)" })
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: ".trig6",
+})
+  .setTween(".svgCont", 1, { backgroundColor: "rgb(33, 85, 56)" })
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: ".trig6",
+})
+  .setTween(".mainImg", 1, { backgroundColor: "rgb(83, 235, 151)" })
+  .addTo(controller);
